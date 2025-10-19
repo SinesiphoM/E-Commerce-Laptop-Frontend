fb1ec73c9d5b96a4c28eb055bc9500b5284763d5
Nontando Zondi <221248986@mycput.ac.za>
2025-10-19 21:37:00 +0200

    Initial commit: React ecommerce project setup

---
+++ src/components/main/Background.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Background() {
+  return (
+    <div
+      className="absolute inset-0 -z-10"
+      style={{
+        backgroundImage:
+          'url(https://images.unsplash.com/photo-1517336714731-489689fd1ca8?auto=format&fit=crop&w=1200&q=80)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        opacity: 0.25,
+      }}
+    />
+  );
+}
---
+++ src/utils/authFetch.js
@@ -0,0 +1,18 @@
+const BASE_URL = 'http://localhost:8080';
+
+export async function authFetch(endpoint, options = {}) {
+  try {
+    let headers = options.headers || {};
+    // Only set Content-Type to application/json if body is not FormData
+    if (!(options.body instanceof FormData)) {
+      headers = { 'Content-Type': 'application/json', ...headers };
+    }
+    const res = await fetch(BASE_URL + endpoint, {
+      ...options,
+      headers
+    });
+    return res;
+  } catch (err) {
+    throw new Error('Network error');
+  }
+}
